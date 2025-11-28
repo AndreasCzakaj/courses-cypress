f2d5ff6f47c4344130f41cf05e0f4c0d82867359
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-11-28 11:06:39 +0100

    initial challenge state

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,8 @@
 module.exports = {
+  viewportWidth: 1440,
+  viewportHeight: 660,
   e2e: {
+    testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/cop-xxl-tests.cy.js
@@ -0,0 +1,35 @@
+describe("CoP QA XXL", () => {
+  // Before all tests: visit the site and accept cookie consent
+  before(() => {
+    cy.visit("https://www.mercedes-benz-trucks.com");
+
+    // Handle the Usercentrics cookie consent shadow DOM
+    cy.get("#usercentrics-root")
+      .shadow()
+      .within(() => {
+        cy.get("[data-testid=uc-accept-all-button]", {
+          includeShadowDom: true,
+          timeout: 15000,
+        })
+          .should("be.visible")
+          .click({
+            timeout: 15000,
+          });
+      });
+  });
+
+  it.skip("Cypress level 1 - Text is present");
+
+  it.skip("Cypress level 1 - Text is not present");
+
+  it.skip("Cypress level 1 - Text is matching regular expression");
+
+  it.skip("Cypress level 1 - Text is present but not visible");
+
+  it.skip("Cypress level 2 - Validate an image element");
+
+  it.skip("Cypress level 3 - Validate header navigation");
+
+  it.skip("Cypress level 4 - Validate flyout navigation");
+
+});
